1cd4a704ce322f28b3be5a3ced5fb02f404d01f6
anggitaembun <anggita.embun@gmail.com>
2023-12-05 22:19:57 +0700

    Jobsheet 6 (Fragment)

---
+++ app/src/main/java/com/example/jobsheet6/api/ApiConfig.kt
@@ -0,0 +1,19 @@
+package com.example.jobsheet6.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+object ApiConfig {
+    const val baseURL = "https://rickandmortyapi.com/api/"
+
+    fun getRetrofit() : Retrofit {
+        return Retrofit.Builder().baseUrl(baseURL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    fun getService() : Apiservice{
+        return getRetrofit() .create(Apiservice::class.java)
+    }
+}
